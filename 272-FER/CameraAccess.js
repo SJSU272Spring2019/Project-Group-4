9173b553ea5c61ed9d1c62898e1ffa6f89e1df88
Jaya Sindhu <jayasindhu.manda@sjsu.edu>
2019-04-18 15:48:09 -0700

    Hitting API with image content

--- 272-FER/CameraAccess.js
+++ 272-FER/CameraAccess.js
@@ -6,6 +6,20 @@ function triggerCamera(){
  }
 }
 
+function blobToBase64(blob, cb) {
+  var reader = new window.FileReader();
+  reader.readAsDataURL(blob); 
+  reader.onloadend = function() {           
+    cb(reader.result);
+  }
+}
+
+function canvasToBase64(canvas, cb) {
+  canvas.toBlob(function(blob) {
+    blobToBase64(blob, cb);
+  }, 'image/jpeg');
+}
+
 function cameraTrigger(){
 	var video = document.getElementById('video');
 	var canvas = document.getElementById('canvas');
@@ -35,4 +49,54 @@ function snap() {
 	var context = canvas.getContext('2d');
 	context.drawImage(video, 0, 0);
 	video.style = "display:none";
+	imageUpload(canvas)
 }
+
+function redo(){
+	var video = document.getElementById('video');
+	var canvas = document.getElementById('canvas');
+	video.style = "visibility:visible";
+	canvas.style="display:none";
+	setTimeout(function(){ snap();}, 3000);
+}
+
+function imageUpload(canvas) {
+    canvasToBase64(canvas, function(b64) {
+      b64 = b64.replace('data:image/jpeg;base64,', ''); // remove content type
+      request = {
+        "requests":[
+          {
+            "image":{ "content": b64 },
+            "features":[
+              {
+                "type":"FACE_DETECTION",
+                "maxResults":1
+              }
+            ]
+          }
+        ]
+      };
+      
+      $.ajax({
+        method: 'POST',
+        url: 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBsiG1WCYi8thWKkmL4Li1LmuDXqK4eezY',
+        contentType: 'application/json',
+        data: JSON.stringify(request),
+        processData: false,
+        success: function(data){
+          output = data;
+          var faceData = data.responses[0].faceAnnotations[0];
+          console.log('joy: ' + faceData.joyLikelihood);
+          console.log('sorrow: ' + faceData.sorrowLikelihood);
+          console.log('anger: ' + faceData.angerLikelihood);
+          console.log('surprise: ' + faceData.surpriseLikelihood);
+        },
+        error: function (data, textStatus, errorThrown) {
+          console.log('error: ' + data);
+        }
+      })
+    })
+}
+
+
+
